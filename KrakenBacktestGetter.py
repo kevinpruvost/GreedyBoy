df47636e54a52328df405c19588de680ed9ff02b
Kevin Pruvost <kevin.pruvost@epitech.eu>
2021-04-22 19:14:36 +0200

    backtesting getter now takes ETH, XDG, XBT & XRP

--- KrakenBacktestGetter.py
+++ KrakenBacktestGetter.py
@@ -9,6 +9,8 @@ import time, base64, hashlib, hmac, urllib, json
 import tempfile
 from github import Github
 
+currencyInitials = ["XDG", "XBT", "ETH", "XRP"]
+
 class KrakenBacktestGetter:
     ##
     ## AUTHENTIFICATION
@@ -36,33 +38,42 @@ class KrakenBacktestGetter:
     ##
 
     def ws_thread(self, *args):
-        try:
-            githubFile = self.greedyBoyRepo.get_contents(self.githubDataPath, self.branchName)
-            githubFileContent = githubFile.decoded_content.decode('ascii')
-            empty = not csv.Sniffer().has_header(githubFileContent)
-            if not empty:
-                self.dataFile = open(self.dataPath, "w")
-                self.dataFile.write(githubFileContent)
+        self.dataFiles = dict()
+        self.dataWriters = dict()
+        for i in range(len(currencyInitials)):
+            try:
+                self.dataFiles[currencyInitials[i]] = None
+                self.dataWriters[currencyInitials[i]] = None
+                githubFile = self.greedyBoyRepo.get_contents(self.githubDataPaths[i], self.branchName)
+                githubFileContent = githubFile.decoded_content.decode('ascii')
+                empty = not csv.Sniffer().has_header(githubFileContent)
+                if not empty:
+                    self.dataFile = open(self.dataPaths[i], "w")
+                    self.dataFile.write(githubFileContent)
+                    self.dataFile.close()
+            except:
+                empty = True
+                self.dataFile = open(self.dataPaths[i], "w")
+                self.dataFile.write("")
                 self.dataFile.close()
-        except:
-            empty = True
-            self.dataFile = open(self.dataPath, "w")
-            self.dataFile.write("")
-            self.dataFile.close()
-        self.dataFile = open(self.dataPath, "a")
-        self.dataWriter = csv.DictWriter(self.dataFile, fieldnames=["epoch", "price"], lineterminator="\n")
-        if empty:
-            self.dataWriter.writeheader()
+            self.dataFiles[currencyInitials[i]] = open(self.dataPaths[i], "a")
+            self.dataWriters[currencyInitials[i]] = csv.DictWriter(self.dataFiles[currencyInitials[i]], fieldnames=["epoch", "price"], lineterminator="\n")
+            if empty:
+                self.dataWriters[currencyInitials[i]].writeheader()
 
         def ws_message(ws, message):
             j = json.loads(message)
-            if isinstance(j, list) and j[-1] == "XDG/EUR":
-                for info in j[1]:
-                    self.dataWriter.writerow({"epoch": str(info[2]), "price": str(info[0])})
-                    print("XDG/EUR [" + time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(float(info[2]))) + "]: " + info[0] + " euros.")
+            #print(j)
+            for initial in currencyInitials:
+                initialEur = initial + "/EUR"
+                if isinstance(j, list) and j[-1] == initialEur:
+                    for info in j[1]:
+                        self.dataWriters[initial].writerow({"epoch": str(info[2]), "price": str(info[0])})
+                        print(initialEur + "[" + time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(float(info[2]))) + "]: " + info[0] + "€")
 
         def ws_open(ws):
-            ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["XDG/EUR"]}')
+            for initial in currencyInitials:
+                ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["' + initial + '/EUR"]}')
 
         ws = websocket.WebSocketApp("wss://ws.kraken.com/", on_open=ws_open, on_message=ws_message)
         ws.run_forever()
@@ -72,42 +83,44 @@ class KrakenBacktestGetter:
     ##
 
     def close(self):
-        if not self.dataFile.closed:
-            self.dataFile.close()
-        self.dataFile = open(self.dataPath, "r")
-        cnt = self.dataFile.read()
-
         timer = time.perf_counter()
 
-        update = False
-        try:
-            dirContent = self.greedyBoyRepo.get_dir_contents("./price_history", self.branchName)
+        for i, initial in enumerate(currencyInitials):
+            if not self.dataFiles[initial].closed:
+                self.dataFiles[initial].close()
+            self.dataFiles[initial] = open(self.dataPaths[i], "r")
+            cnt = self.dataFiles[initial].read()
+            self.dataFiles[initial].close()
 
-            ## Checks if file already exists
-            for file in dirContent:
-                if file.name == self.githubDataFilename:
-                    update, fileSha = True, file.sha
-                    break
-        except: 0
+            update = False
+            try:
+                dirContent = self.greedyBoyRepo.get_dir_contents("./price_history/" + initial, self.branchName)
 
-        msg = self.githubDataFilename + " updated."
-        if update:
-            self.greedyBoyRepo.update_file(
-                path=self.githubDataPath,
-                message=msg,
-                content=cnt,
-                branch=self.branchName,
-                sha=file.sha
-            )
-        else:
-            self.greedyBoyRepo.create_file(
-                path=self.githubDataPath,
-                message=msg,
-                content=cnt,
-                branch=self.branchName
-            )
+                ## Checks if file already exists
+                for file in dirContent:
+                    if file.name == self.githubDataFilename:
+                        update, fileSha = True, file.sha
+                        break
+            except: 0
 
-        print("Sending file took " + str(time.perf_counter() - timer) + " seconds.")
+            msg = self.githubDataFilename + " updated."
+            if update:
+                self.greedyBoyRepo.update_file(
+                    path=self.githubDataPaths[i],
+                    message=msg,
+                    content=cnt,
+                    branch=self.branchName,
+                    sha=file.sha
+                )
+            else:
+                self.greedyBoyRepo.create_file(
+                    path=self.githubDataPaths[i],
+                    message=msg,
+                    content=cnt,
+                    branch=self.branchName
+                )
+
+        print("Sending files took " + str(time.perf_counter() - timer) + " seconds.")
 
 
     ##
@@ -115,9 +128,9 @@ class KrakenBacktestGetter:
     ##
 
     def __init__(self, apiKey, apiPrivateKey, githubToken, repoName, dataBranchName):
-        self.dataPath = tempfile.gettempdir() + "/data.csv"
+        self.dataPaths = [tempfile.gettempdir() + "/data" + initial + ".csv" for initial in currencyInitials]
         self.githubDataFilename = time.strftime('%d-%m-%Y', time.localtime(time.time())) + ".csv"
-        self.githubDataPath = "./price_history/" + self.githubDataFilename
+        self.githubDataPaths = ["./price_history/" + initial + "/" + self.githubDataFilename for initial in currencyInitials]
 
         self.apiKey, self.apiPrivateKey = apiKey, apiPrivateKey
         self.githubToken, self.branchName = githubToken, dataBranchName

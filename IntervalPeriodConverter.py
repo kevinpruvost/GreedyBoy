983e16f9440b0c7f30175e56fce6d437821eae49
Kevin Pruvost <kevin.pruvost@epitech.eu>
2021-04-25 16:40:10 +0200

    intervalperiod first success
    realtime display and Bollinger Bands are the last things to make

--- IntervalPeriodConverter.py
+++ IntervalPeriodConverter.py
@@ -84,3 +84,36 @@ data.append(data.ordered.at[len(data.ordered) - 1, 'Date'] + 15 * 60, 2100)
 data.ordered["Date"] = pd.to_datetime(data.ordered["Date"], unit='s')
 data.ordered = data.ordered.set_index('Date')
 print(data.ordered)
+
+import csv
+import matplotlib.pyplot as plt
+import mplfinance
+import pandas as pd
+import matplotlib.dates as mpl_dates
+import matplotlib.animation as animation
+import time
+
+plt.style.use('ggplot')
+
+idf = data.ordered
+df = idf.loc['2021-04-24 00:00:00':'2021-04-24 12:00:00',:]
+
+fig = mplfinance.figure(figsize=(11,5))
+ax1 = fig.add_subplot(2, 1, 1)
+ax2 = fig.add_subplot(3, 1, 3)
+
+def animate(ival):
+    if (20+ival) > len(df):
+        print('no more data to plot')
+        ani.event_source.interval *= 3
+        if ani.event_source.interval > 12000:
+            exit()
+        return
+    datas = df.iloc[0:(20+ival)]
+    ax1.clear()
+    mplfinance.plot(idf, ax=ax1, type='candle', style='charles')
+    mplfinance.plot(datas, ax=ax2, type='candle', style='charles')
+
+ani = animation.FuncAnimation(fig, animate, interval=250)
+
+mplfinance.show()

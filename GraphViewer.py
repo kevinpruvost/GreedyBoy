6a0ad20d014d0461ac410e9532fc4020fe438698
Kevin Pruvost <kevin.pruvost@epitech.eu>
2021-04-30 10:46:05 +0200

    graphviewer cleaned up and commented

--- GraphViewer.py
+++ GraphViewer.py
@@ -17,7 +17,6 @@ __maintainer__  = "Kevin Pruvost"
 __email__       = "pruvostkevin0@gmail.com"
 __status__      = "Test"
 
-import numpy as np
 import matplotlib.pyplot as plt
 import mplfinance
 import matplotlib.animation as animation
@@ -25,9 +24,15 @@ import mplcursors
 
 class GraphViewer:
     def __init__(self, priceDatas, bollingerDatas, animateCallback = None, fullscreen: bool = True):
-        fig = mplfinance.figure(figsize=(15, 7))
-        ax1 = fig.add_subplot(2, 1, 1)
-        ax2 = fig.add_subplot(2, 1, 2)
+        """Constructs the GraphViewer
+        :param priceDatas: Dataframe containing detailed informations about prices.
+        :param bollingerDatas: Dataframe containing informations about bollinger gaps.
+        :param animateCallback: Callback called on the animation loop.
+        :param fullscreen: De/Activates fullscreen mode for Matplotlib.
+        """
+        fig = mplfinance.figure(figsize=(15, 7))    # Defining figure size
+        ax1 = fig.add_subplot(2, 1, 1)              # Defining plot 1
+        ax2 = fig.add_subplot(2, 1, 2)              # Defining plot 2
 
         idf, df = priceDatas, bollingerDatas
         bollinger_bands = idf[['HBand', 'LBand']]
@@ -41,37 +46,42 @@ class GraphViewer:
                     exit()
                 return
             # datas = df.iloc[0:(20+ival)]
-            ax1.clear()
-            mplfinance.plot(idf, ax=ax1, type='candle', style='charles')
-            slt = bollinger_bands.plot(ax=ax1, use_index=False)
-            fm = plt.get_current_fig_manager()
 
-            # ax1.fill_between(bollinger_bands.index, bollinger_bands['HBand'], bollinger_bands['LBand'], color='grey', alpha=0.5)
+            ##
+            ## Drawing Price Candle Chart + Bollinger Bands
+            ##
+            ax1.clear()     # Clear
+            mplfinance.plot(idf, ax=ax1, type='candle', style='charles')    # Drawing Candle Chart
+            chart1 = bollinger_bands.plot(ax=ax1, use_index=False)          # Drawing Bollinger Bands
 
-            ax2.cla()
+            ##
+            ## Drawing Bollinger Gaps
+            ##
+            ax2.cla()           # Clear
+            df.plot(ax=ax2)     # Drawing data lines
+            ax2.axhline(y=100, color="red", lw=1, linestyle=":")    # Drawing '100' line limit
+            ax2.axhline(y=0, color="green", lw=1, linestyle=":")    # Drawing '0' line limit
 
-            upper = 100
-            lower = 0
-            supper = np.ma.masked_where(df['Value'] < upper, df['Value'])
-            slower = np.ma.masked_where(df['Value'] > lower, df['Value'])
-            smiddle = np.ma.masked_where((df['Value'] < lower) | (df['Value'] > upper), df['Value'])
-
-            df.plot(ax=ax2)
-            ax2.axhline(y=100, color="red", lw=1, linestyle=":")
-            ax2.axhline(y=0, color="green", lw=1, linestyle=":")
+            # Defining scatter points color considering their values
+            # <= 0      : Green,
+            # >= 100    : Red,
+            # Otherwise : Blue
             colors = ['#00a822' if val <= 0 else 'r' if val >= 100 else '#00000033' for val in df['Value']]
-            slt2 = ax2.scatter(df.index, df['Value'], color=colors)
-            # ax2.plot(df.index, df.index, '-r')
+            chart2 = ax2.scatter(df.index, df['Value'], color=colors)
 
-            mplcursors.cursor(slt, hover=True)
-            mplcursors.cursor(slt2, hover=True)
+            # Activating cursor interactions
+            mplcursors.cursor(chart1, hover=True)
+            mplcursors.cursor(chart2, hover=True)
 
-        ani = animation.FuncAnimation(fig, animate, interval=1000)
+        ani = animation.FuncAnimation(fig, animate, interval=1000)  # Creating Animation
 
+        ##
+        ## Customizing Matplotlib
+        ##
         figManager = plt.get_current_fig_manager()
         if fullscreen: figManager.full_screen_toggle()
-
         plt.subplots_adjust(left=0.04, bottom=0.067, right=0.93, top=0.955)
+
         self.start()
 
     def start(self):
